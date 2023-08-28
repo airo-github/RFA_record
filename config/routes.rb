ba44315f22f8adbd26b187a981464fd6e8de5983
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-08-28 14:50:27 +0900

    add: 簡単なLINEログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#top'
+  get '/after_login', to: 'static_pages#after_login'
+  resource :user, only: %i[new create]
 end
