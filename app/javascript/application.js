b4c5561a20325344a80cc3574189fc5bddb693ff
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-09-17 21:31:32 +0900

    :bug: Fix バグを修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@ import * as bootstrap from "bootstrap"
 import './preview'
 import "@hotwired/turbo-rails"
 import "./controllers"
-import './new'
 import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
