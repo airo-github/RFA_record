457f4b2b2003f2d4907aedd3fe8c3c5da7deea9f
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-09-17 17:58:10 +0900

    :bug: Fix JSの読み込みを修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,7 @@ import './preview'
 import "@hotwired/turbo-rails"
 import "./controllers"
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus/webpack-helpers"
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
