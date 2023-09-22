3598a2d34851c1c6583dee473d066a5a9d2338c9
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-09-22 19:46:37 +0900

    :zap: OGPの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'RFA_Record',
+      title: 'RFAの活動記録を保存するサービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: '写真を撮るだけで活動記録が簡単に保存、グラフ化できます',
+      keywords: 'リングフィットアドベンチャー',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('RFA_Record_OGP.jpeg'), # 配置するパスやファイル名によって変更すること
+        local: 'ja-JP'
+      }
+    }
+  end
 end
