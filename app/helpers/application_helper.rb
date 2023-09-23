971151b2fa0d7b0bc7adef25b80108a566208a39
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-09-23 14:38:54 +0900

    :bug: バグ修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,11 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'RFA_Record',
-      title: 'RFAの活動記録を保存するサービス',
+      title: 'ZooMania',
       reverse: true,
       charset: 'utf-8',
-      description: '写真を撮るだけで活動記録が簡単に保存、グラフ化できます',
-      keywords: 'リングフィットアドベンチャー',
+      description: '動物園に行こう！',
+      keywords: '動物,動物園,動物園一覧',
       canonical: request.original_url,
       separator: '|',
       og: {
@@ -15,8 +14,13 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('RFA_Record_OGP.jpeg'),
+        image: image_url('ogp.jpeg'),
         local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@RYOYA_RUNTEQ44',
+        image: image_url('ogp.jpeg')
       }
     }
   end
