1c857e53f9755c4a027965e19f744d76ffd044cf
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-10-05 15:23:39 +0900

    :recycle: ActivityImageコントローラーをリファクタリング

--- app/models/activity_image.rb
+++ app/models/activity_image.rb
@@ -4,4 +4,20 @@ class ActivityImage < ApplicationRecord
   mount_uploader :image, ActivityImageUploader
 
   validates :image, presence: true
+
+  def extract_and_parse_ocr_text
+    response = Google::Cloud::Vision.image_annotator.document_text_detection(
+      image: image.url, max_results: 1, image_context: { language_hints: %i[ja en] }
+    )
+    ocr_text = response.responses[0].text_annotations[0].description
+
+    min_sec = ocr_text.match(/(\d+分\s*\d+\s*秒)/)[0] # '〇〇分〇〇秒'を抽出
+
+    {
+      min: min_sec.match(/(\d+)分/)[1].to_i, # 分を抽出・数値化
+      sec: min_sec.match(/(\d+\s*)秒/)[1].to_i, # 秒を抽出・数値化
+      kcal: ocr_text.match(/(\d+\.\d+kcal)/)[0].gsub(/kcal/, '').to_f, # '〇〇.〇〇kcal'を抽出・数値化
+      km: ocr_text.match(/(\d+\.\d+km)/)[0].gsub(/km/, '').to_f # '〇〇.〇〇km'を抽出・数値化
+    }
+  end
 end
--- app/models/activity_record.rb
+++ app/models/activity_record.rb
@@ -7,4 +7,22 @@ class ActivityRecord < ApplicationRecord
   validates :act_calories, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :act_distance, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :comment, length: { maximum: 20 }
+
+  def self.create_new_record(image, extracted_info, current_user)
+    act_time = extracted_info[:min] * 60 + extracted_info[:sec]
+    act_date = Date.today
+    act_calories = extracted_info[:kcal]
+    act_distance = extracted_info[:km]
+
+    activity_record = ActivityRecord.new(
+      act_date:,
+      act_time:,
+      act_calories:,
+      act_distance:,
+      user_id: current_user.id,
+      activity_image_id: image.id
+    )
+
+    activity_record.save ? activity_record : nil
+  end
 end
