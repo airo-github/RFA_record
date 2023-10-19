7b2370ba4b273d65559093ff44f0645f3fadb2d1
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-10-19 17:54:57 +0900

    :bug: バグ修正

--- app/models/activity_image.rb
+++ app/models/activity_image.rb
@@ -9,20 +9,15 @@ class ActivityImage < ApplicationRecord
     response = Google::Cloud::Vision.image_annotator.document_text_detection(
       image: image.url, max_results: 1, image_context: { language_hints: %i[ja en] }
     )
-
     ocr_text = response.responses[0].text_annotations[0].description
 
-    min_sec = ocr_text.match(/(\\d+分\\s*\\d+\\s*秒)/)&.first
-    min = min_sec&.match(/(\\d+)分/)&.to_a&.dig(1)&.to_i || 0
-    sec = min_sec&.match(/(\\d+\\s*)秒/)&.to_a&.dig(1)&.to_i || 0
-    kcal = ocr_text.match(/(\\d+\\.\\d+kcal)/)&.to_a&.dig(0)&.gsub(/kcal/, '')&.to_f || 0.0
-    km = ocr_text.match(/(\\d+\\.\\d+km)/)&.to_a&.dig(0)&.gsub(/km/, '')&.to_f || 0.0
+    min_sec = ocr_text.match(/(\d+分\s*\d+\s*秒)/)[0] # '〇〇分〇〇秒'を抽出
 
     {
-      min:,
-      sec:,
-      kcal:,
-      km:
+      min: min_sec.match(/(\d+)分/)[1]&.to_i || 0,
+      sec: min_sec.match(/(\d+\s*)秒/)[1]&.to_i || 0,
+      kcal: ocr_text.match(/(\d+\.\d+kcal)/)[0].gsub(/kcal/, '')&.to_f || 0.0,
+      km: ocr_text.match(/(\d+\.\d+km)/)[0].gsub(/km/, '')&.to_f || 0.0
     }
   end
 end
