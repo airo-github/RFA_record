72e8069e22f74f9f0b82fce78baa25c3f52d7552
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-10-27 11:20:31 +0900

    :zap: セッションの有効期限を1日に設定

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_user_id_session', expire_after: 1.month
+Rails.application.config.session_store :cookie_store, key: '_user_id_session', expire_after: 1.day
